21459ff92411b39cff91fb679d97a5c30f4aa34e
Erik <eculberson12@gmail.com>
2017-10-30 16:11:33 -0400

    Adding views

--- Project 1/dimensionality_reduction.r
+++ Project 1/dimensionality_reduction.r
@@ -11,3 +11,4 @@ dimensional_reduction <- function(data, labels) {
 
   return(pca)
 }
+
--- Project 1/load_data.r
+++ Project 1/load_data.r
@@ -5,3 +5,26 @@ test_data <- read.table("./UCI HAR Dataset/test/X_test.txt")
 test_labels <- read.table("./UCI HAR Dataset/test/y_test.txt")
 print(dim(training_data))
 print(dim(training_labels))
+training_labels <- data.frame(class=training_labels[,1])
+training_set <- cbind(training_data, training_labels)
+
+print(dim(training_set[training_set$class==1,]))
+print(dim(training_set[training_set$class==2,]))
+print(dim(training_set[training_set$class==3,]))
+print(dim(training_set[training_set$class==4,]))
+print(dim(training_set[training_set$class==5,]))
+print(dim(training_set[training_set$class==6,]))
+
+t1 <- training_set[training_set$class==1,]
+t2 <- training_set[training_set$class==2,]
+t3 <- training_set[training_set$class==3,]
+t4 <- training_set[training_set$class==4,]
+t5 <- training_set[training_set$class==5,]
+t6 <- training_set[training_set$class==6,]
+
+plot(t(as.matrix(t1[1,])), type="l", col="red")
+lines(t(as.matrix(t2[1,])), type = "l", col = "blue")
+lines(t(as.matrix(t3[1,])), type = "l", col = "orange")
+lines(t(as.matrix(t4[1,])), type = "l", col = "green")
+lines(t(as.matrix(t5[1,])), type = "l", col = "yellow")
+lines(t(as.matrix(t6[1,])), type = "l", col = "pink")
